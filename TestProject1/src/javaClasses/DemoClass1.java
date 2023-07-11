5fc8b6a01ce6aae0904a8c625be4094ad59ee8ef
dtyagi <dtyagi@ITEM-S119790.emea.msad.sopra>
2023-07-11 13:15:40 +0530

    Commit and push - DemoClass1.java

---
+++ TestProject1/src/javaClasses/DemoClass1.java
@@ -0,0 +1,15 @@
+package javaClasses;
+
+public class DemoClass1 {
+
+	public static void main(String args[])
+	{
+		DemoClass1 dc1 = new DemoClass1();
+		dc1.hello();
+	}
+	
+	public void hello()
+	{
+		System.out.println("Hello World");
+	}
+}
